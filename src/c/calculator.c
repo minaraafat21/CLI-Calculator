931b90a4c607b5f845fc2690c4926c34148990e9
Kirellos-Safwat <kero.safwat763@gmail.com>
2025-06-13 08:02:45 +0300

    chore: apply final clang-format fixes and pass all pre-commit hooks

--- src/c/calculator.c
+++ src/c/calculator.c
@@ -6,7 +6,7 @@ double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
 double multiply(double a, double b) { return a * b; }
 double divide(double a, double b) {
-  if (b == 0) {  // handle zero division
+  if (b == 0) { // handle zero division
     printf("Error: Division by zero is not allowed.\n");
     // it will also raise a python error before even being passed here
   }
